626a4f3b6f0b8b0e5c7845afe17a384a178d3410
kenni <WizardOfFailure@hotmail.com>
2025-07-09 12:11:10 +0200

    All setup finsihed

---
+++ src/main/java/com/example/crimestream/CrimeStreamApplication.java
@@ -0,0 +1,13 @@
+package com.example.crimestream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimeStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrimeStreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crimestream/CrimeStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crimestream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimeStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
